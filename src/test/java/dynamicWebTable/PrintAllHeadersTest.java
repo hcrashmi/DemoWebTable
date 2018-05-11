3418ca8f93cdc9065015c10c5245e6697c0d4e08
Shcrashmi <37067788+hcrashmi@users.noreply.github.com>
2018-05-11 16:09:42 +0530

    hiiii

--- src/test/java/dynamicWebTable/PrintAllHeadersTest.java
+++ src/test/java/dynamicWebTable/PrintAllHeadersTest.java
@@ -26,11 +26,12 @@ public class PrintAllHeadersTest
 			System.out.println(header.getText());
 		}
 		System.out.println("application will close");
-<<<<<<< HEAD
+
 		
 		System.out.println("application will close now");
-=======
->>>>>>> branch 'master' of https://github.com/hcrashmi/DemoWebTable.git
+
+
+System.out.println("hiiiiiiiii");
 		System.out.println("application  close");
 		driver.quit();
 		}
